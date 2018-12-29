d6ee4fec2ad1827b15d119c2d0743a7dc9d500a4
Nithin Mahesh <nithin11197@gmail.com>
2018-12-28 19:22:37 -0500

    Initial Commit

---
+++ Hot Spot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hot Spot
+//
+//  Created by nithin mahesh on 12/28/18.
+//  Copyright © 2018 nithin mahesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
