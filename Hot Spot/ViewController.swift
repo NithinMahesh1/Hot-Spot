8c41297e1ce59b4ad823019bf4c1e8dfc22cd59b
Nithin Mahesh <nithin11197@gmail.com>
2018-12-28 20:25:05 -0500

    Beginning of SQLite integration for user DB

--- Hot Spot/ViewController.swift
+++ Hot Spot/ViewController.swift
@@ -15,6 +15,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func createTable() {
+        print("CREATE TAPPED")
+    }
+    
+    @IBAction func insertUser() {
+        print("INSERT TAPPED")
+        let alert = UIAlertController(title: "Insert User", message: nil, preferredStyle: .alert)
+            alert.addTextField { (tf) in tf.placeholder = "Name"}
+            alert.addTextField { (tf) in tf.placeholder = "Email"}
+        let action = UIAlertAction(title: "Submit", style: .default) { (_) in
+        guard let userIDString = alert.textFields?.first?.text,
+            let email = alert.textFields?.last?.text
+                else {return}
+            print(userIDString)
+            print(email)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func listUsers() {
+        print("LIST TAPPED")
+    }
+    
+    
+    
+    
+    
 }
 
